bfb6b1d43c58ff6ecdd53db8d73cb3e33351fd4a
Joël <you@example.com>
2023-11-07 17:34:13 +0100

    début optimal policy

--- pickomino_project.py
+++ pickomino_project.py
@@ -11,47 +11,47 @@ class Action:
 
 class State:
 
-    def __init__(self, keptDice, remainingDice):
-        # keptDice : list of length 6 that indicates how many dice i the player has kept
-        # remainingDice : similar
-        assert(len(keptDice) == 6 and len(remainingDice) == 6)
-        assert(sum(keptDice) + sum(remainingDice) == 8)
+    def __init__(self, keptDices : list, remainingDices : list):
+        # keptDices : list of length 6 that indicates how many dices i the player has kept
+        # remainingDices : similar
+        assert(len(keptDices) == 6 and len(remainingDices) == 6)
+        assert(sum(keptDices) + sum(remainingDices) == 8)
         # nbKept : nbKept : the player keeps the first nbKept bits for the moment
-        self.keptDice = keptDice # TODO maybe copy ?
-        self.remainingDice = remainingDice
+        self.keptDices = keptDices # TODO maybe copy ?
+        self.remainingDices = remainingDices
 
         self.is_final = True
         for i in range(6):
-            if self.keptDice[i] == 0 or self.remainingDice[i] != 0:
+            if self.keptDices[i] == 0 or self.remainingDices[i] != 0:
                 self.is_final = False
                 # a dice can be picked, so state is not final
                 break
     
     def __str__(self):
-        keptDice = []
-        remainingDice = []
+        keptDices = []
+        remainingDices = []
         for dice_value in range(6):
-            for j in range(self.keptDice[dice_value]):
-                keptDice.append(dice_value + 1)
-            for j in range(self.remainingDice[dice_value]):
-                remainingDice.append(dice_value + 1)
-        return "Kept dice : " + str(keptDice) + " Remaining dice " + str(remainingDice)
+            for j in range(self.keptDices[dice_value]):
+                keptDices.append(dice_value + 1)
+            for j in range(self.remainingDices[dice_value]):
+                remainingDices.append(dice_value + 1)
+        return "Kept dices : " + str(keptDices) + " Remaining dices " + str(remainingDices)
             
-    def is_final(self):
+    def is_final_state(self):
         # returns true iff state is final ; that is no dice can be picked
         # the boolean value is_final is computed at init for performance reasons
         return self.is_final
 
     def get_kept_value(self):
         # assuming player chooses to stop immediately, how many points do they have ?
-        if self.keptDice[5] == 0: # player did not keep a worm : failed !
+        if self.keptDices[5] == 0: # player did not keep a worm : failed !
             return Rewards.C
         if self.is_final:
             return Rewards.C
 
         reward = 0
         for dice_value in range(6):
-            reward += self.keptDice[dice_value] * (dice_value + 1)
+            reward += self.keptDices[dice_value] * (dice_value + 1)
         if reward < 21:
             return Rewards.C
         
@@ -64,17 +64,21 @@ def reward(s : State, a):
     
     return s.get_kept_value()
 
-def enumerate_lists(total, length):
-    if length == 1:
-        yield [total]
-    else:
-        for i in range(total + 1):
-            for l in enumerate_lists(total - i, length - 1):
-                l.append(i)
-                yield l
+def compute_opt_policy():
+    # first, compute reward and action (STOP) for every final state
+    policy = {} # maps a state to a (action, reward) pair
 
+    for k in range(9):
+        for keptDices in utils.generate_distributions(k):
+            for remainingDices in utils.generate_distributions(8 - k):
 
-i = 0
-for l in enumerate_lists(8, 12):
-    i += 1
-print(i)
+                s = State(keptDices, remainingDices)
+
+                if s.is_final_state():
+                    a = Action.STOP
+                    policy[s] = (a, reward(s, a))
+    
+    
+    print("finished !")
+
+compute_opt_policy()
