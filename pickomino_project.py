c1a448a0df5018a6e625f7e7aeb731effcac43d2
Arthur Dumas <arthur.dumas@ens-rennes.fr>
2023-11-07 18:08:39 +0100

    File reorganisation

--- pickomino_project.py
+++ pickomino_project.py
@@ -70,23 +70,3 @@ def reward(s : State, a):
         return 0 # reward is 0 for intermediate states (where player chooses to continue)
     
     return s.get_kept_value()
-
-def compute_opt_policy(s : State):
-    # first, compute reward and action (STOP) for every final state
-    
-    # policy = {} # maps a state to a (action, reward) pair
-
-    # for k in range(9):
-    #     for keptDices in utils.generate_distributions(k):
-    #         for remainingDices in utils.generate_distributions(8 - k):
-
-    #             s = State(keptDices, remainingDices)
-
-    #             if s.is_final_state():
-    #                 a = Action.STOP
-    #                 policy[s] = (a, reward(s, a))
-    
-    
-    print("finished !")
-
-compute_opt_policy()
---
+++ policy_computation.py
@@ -0,0 +1,22 @@
+import pickomino_project
+import utils
+
+def compute_opt_policy(s : State):
+    # first, compute reward and action (STOP) for every final state
+    
+    # policy = {} # maps a state to a (action, reward) pair
+
+    # for k in range(9):
+    #     for keptDices in utils.generate_distributions(k):
+    #         for remainingDices in utils.generate_distributions(8 - k):
+
+    #             s = State(keptDices, remainingDices)
+
+    #             if s.is_final_state():
+    #                 a = Action.STOP
+    #                 policy[s] = (a, reward(s, a))
+    
+    
+    print("finished !")
+
+compute_opt_policy()
